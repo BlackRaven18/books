f1b59d23957858fc6f207041a69fb77425e63041
Bartosz <bartoszk00@o2.pl>
2023-01-25 16:09:12 +0100

    Dzialajaca rejestracja

--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -3,10 +3,11 @@ import React, { useEffect, useState } from "react";
 import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import { collection, getDocs, getFirestore, set, doc, firestore, firebase, updateDoc, setDoc } from "firebase/firestore";
+import { collection, getDocs, getFirestore, set, doc, firestore, firebase, updateDoc, setDoc, query, where } from "firebase/firestore";
 import app from "../firestoreConfig";
+import LoggedUserManager from '../LoggedUserManager';
 
-export default function RegistrationScreen({navigation}) {
+export default function RegistrationScreen({ navigation }) {
 
     const [displayName, setDisplayName] = useState('');
     const [displaySurname, setDisplaySurname] = useState('');
@@ -16,6 +17,7 @@ export default function RegistrationScreen({navigation}) {
     const [errorMessage, setErrorMessage] = useState(null);
 
     const [data, setData] = useState([])
+
 
     const db = getFirestore(app);
     useEffect(() => {
@@ -45,26 +47,69 @@ export default function RegistrationScreen({navigation}) {
                 console.log(error.message)
                 // ..
             });
-        navigation.navigate("Logowanie", {language: "english"})
+        navigation.navigate("Logowanie", { language: "english" })
     };
 
+    const favoriteData = {
+                x: "xxx"
+     };
+     const reservationData = {
+                     x: "xxx"
+          };
+          const historyData = {
+                          x: "xxx"
+               };
+
     const dodajKolekcje = async () => {
+        const loggedUserManager = LoggedUserManager.getInstance();
+        const newUserId = loggedUserManager.getId();
         const userRef = doc(collection(db, "users"));
+
         const data = {
-          imie: displayName,
-          nazwisko: displaySurname,
-          email: email
+            imie: displayName,
+            nazwisko: displaySurname,
+            email: email
         };
-        await setDoc(userRef, data);
+
+
+
+        try {
+            await setDoc(userRef, data);
+            const zmienna=searchUser();
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const searchUser = async () => {
+                    const loggedUserManager = LoggedUserManager.getInstance();
+                    const qRef = query(collection(db, "users"), where("email", "==", email));
+                    const qSnap = await getDocs(qRef);
+                    qSnap.forEach((doc) => {
+                        console.log(doc.id);
+                        loggedUserManager.setId(doc.id);
+                        const wynik=doc.id;
+                        console.log("wynik:"+wynik);
+                        dodajKolekcje2(wynik);
+                    });
+                };
+    const dodajKolekcje2 = async (zmienna) => {
+        console.log("zmienna: "+zmienna);
+        const favoriteRef = doc(collection(db, "users", zmienna, "ulubione"));
+        const reservationRef = doc(collection(db, "users", zmienna, "rezerwacje"));
+        const historyRef = doc(collection(db, "users", zmienna, "historia"));
+        await setDoc(favoriteRef, favoriteData);
+        await setDoc(reservationRef, reservationData);
+        await setDoc(historyRef, historyData);
     }
 
     return (
 
         <View style={styles.container}>
 
-            <Image style={styles.image} source={require("../assets/log2.png")}/>
+            <Image style={styles.image} source={require("../assets/log2.png")} />
             <Text style={styles.mytext}>Rejestracja</Text>
-            <StatusBar style="auto"/>
+            <StatusBar style="auto" />
             <Text style={styles.newtext}>Imię</Text>
             <View style={styles.inputView}>
                 <TextInput
@@ -73,7 +118,7 @@ export default function RegistrationScreen({navigation}) {
                     placeholderTextColor="#acaebd"
                     onChangeText={displayName => setDisplayName(displayName)}
                     value={displayName}
-                    //onChangeText={(login) => setLogin(login)}
+                //onChangeText={(login) => setLogin(login)}
                 />
             </View>
             <Text style={styles.nazwtext}>Nazwisko</Text>
@@ -94,7 +139,7 @@ export default function RegistrationScreen({navigation}) {
                     placeholderTextColor="#acaebd"
                     onChangeText={email => setEmail(email)}
                     value={email}
-                    //onChangeText={(login) => setLogin(login)}
+                //onChangeText={(login) => setLogin(login)}
                 />
             </View>
             <Text style={styles.newtext}>Hasło</Text>
@@ -107,14 +152,14 @@ export default function RegistrationScreen({navigation}) {
                     value={password}
                     maxLength={15}
                     secureTextEntry={true}
-                    //onChangeText={(login) => setLogin(login)}
+                //onChangeText={(login) => setLogin(login)}
                 />
             </View>
 
             <TouchableOpacity style={styles.loginBtn} onPress={handleSignUp}>
                 <Text style={styles.loginText}>Rejestruj</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate("Logowanie", {language: "english"})}>
+            <TouchableOpacity onPress={() => navigation.navigate("Logowanie", { language: "english" })}>
                 <Text style={styles.forgot_button}>Logowanie</Text>
             </TouchableOpacity>
         </View>
