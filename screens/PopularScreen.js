10b492a091dbdfaa60255c1c052c7dad71f16781
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-01-25 12:06:31 +0100

    added different ways to inform about pressure

--- screens/PopularScreen.js
+++ screens/PopularScreen.js
@@ -16,91 +16,111 @@ import app from "../firestoreConfig"
 import { collection, getDocs, addDoc, getDoc, doc } from "firebase/firestore";
 import LoggedUserManager from '../LoggedUserManager';
 
-export default function PopularScreen({navigation}) {
+export default function PopularScreen({ navigation }) {
   const [barometerData, setBarometerData] = useState({});
 
   const loggedUserManager = LoggedUserManager.getInstance();
 
-    useEffect(() => {
-      const subscribe = Barometer.addListener(barometerData => {
-        setBarometerData(barometerData);
+  useEffect(() => {
+    setBarometerData({pressure: 1015});
+
+    // const subscribe = Barometer.addListener(barometerData => {
+    //   setBarometerData(barometerData);
+    // });
+
+
+
+    return () => {
+      //subscribe.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("email z singeltona:" + loggedUserManager.getEmail());
+  }, [])
+
+  const [data, setData] = useState([]);
+
+  const db = getFirestore(app);
+  useEffect(() => {
+    getDocs(collection(db, "popularne")).then((querySnapshot) => {
+      const newData = [];
+      querySnapshot.forEach((doc) => {
+        const docData = doc.data();
+        newData.push({
+          id: doc.id,
+          nazwa: docData.nazwa,
+          nazwad: docData.nazwad,
+          obraz: docData.obraz,
+          obrazd: docData.obrazd,
+        });
+        //rconsole.log(newData);
       });
+      setData(newData);
 
-  
-
-      return () => {
-        subscribe.remove();
-      };
-    }, []);
-
-    useEffect(() => {
-      console.log("email z singeltona:" + loggedUserManager.getEmail());
-    }, [])
-
-    const [data, setData] = useState([]);
-
-            const db = getFirestore(app);
-            useEffect(() => {
-                getDocs(collection(db, "popularne")).then((querySnapshot) => {
-                    const newData = [];
-                    querySnapshot.forEach((doc) => {
-                        const docData = doc.data();
-                        newData.push({
-                            id: doc.id,
-                            nazwa: docData.nazwa,
-                            nazwad: docData.nazwad,
-                            obraz: docData.obraz,
-                            obrazd: docData.obrazd,
-                        });
-                        //rconsole.log(newData);
-                    });
-                    setData(newData);
-
-                });
-            }, []);
+    });
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
-        <Image style={styles.image} source={require("../assets/log2.png")} />
-        <Text style={styles.mytext}>Najpopularniejsze książki</Text>
-        <Text style={styles.mytexta}>{barometerData.pressure} hPa</Text>
-        { barometerData.pressure > 1013 ? (
-                        <Text style={styles.mytexta}>dobre ciśnienie do czytania</Text>
-        ) : null }
-        { barometerData.pressure <= 1013 ? (
-                         <Text style={styles.mytexta}>złe ciśnienie do czytania</Text>
-        ) : null }
-        <FlatList
-                         data={data}
-                         renderItem={(item) => { return(
-                             <View style={styles.sview}>
-                                <View>
-                                 <Image style={styles.imagek} source={{uri: item.item.obraz}} />
+      <Image style={styles.image} source={require("../assets/log2.png")} />
+      <Text style={styles.mytext} >Najpopularniejsze książki</Text>
 
-                                 <Text style={styles.mytextb}>{item.item.nazwa}</Text>
-                                </View>
+      {barometerData.pressure >= 1013 ? (
+        <View>
+          <Text style={styles.goodPressureText}>{barometerData.pressure} hPa</Text>
+          <Text style={styles.goodPressureText}>dobre ciśnienie do czytania</Text>
+        </View>
+      ) : null}
 
-                             </View>
-                         )}}
-                         keyExtractor={item => item.id}
-                         style={styles.flatListStyle}
-                         horizontal={true}
-                     />
-                     <FlatList
-                                              data={data}
-                                              renderItem={(item) => { return(
-                                                  <View style={styles.sview}>
-                                                    <View>
-                                                      <Image style={styles.imagek} source={{uri: item.item.obrazd}} />
+      {barometerData.pressure > 1000 && barometerData.pressure < 1013 ? (
+        <View>
+          <Text style={styles.averagePressureText}>{barometerData.pressure} hPa</Text>
+          <Text style={styles.averagePressureText}>umiarkowane dobre ciśnienie do czytania</Text>
+        </View>
+      ) : null}
+      {barometerData.pressure <= 1000 ? (
+        <View>
+          <Text style={styles.badPressureText}>{barometerData.pressure} hPa</Text>
+          <Text style={styles.badPressureText}>niesprzyjające ciśnienie do czytania</Text>
+        </View>
+      ) : null}
 
-                                                      <Text style={styles.mytextb}>{item.item.nazwad}</Text>
+      <FlatList
+        data={data}
+        renderItem={(item) => {
+          return (
+            <View style={[styles.sview, {marginTop: 10}]}>
+              <View>
+                <Image style={styles.imagek} source={{ uri: item.item.obraz }} />
 
-                                                      </View>
-                                                  </View>
-                                              )}}
-                                              keyExtractor={item => item.id}
-                                              style={styles.flatListStyle}
-                                              horizontal={true}
-                                          />
+                <Text style={styles.mytextb}>{item.item.nazwa}</Text>
+              </View>
+
+            </View>
+          )
+        }}
+        keyExtractor={item => item.id}
+        style={styles.flatListStyle}
+        horizontal={true}
+      />
+      <FlatList
+        data={data}
+        renderItem={(item) => {
+          return (
+            <View style={styles.sview}>
+              <View>
+                <Image style={styles.imagek} source={{ uri: item.item.obrazd }} />
+
+                <Text style={styles.mytextb}>{item.item.nazwad}</Text>
+
+              </View>
+            </View>
+          )
+        }}
+        keyExtractor={item => item.id}
+        style={styles.flatListStyle}
+        horizontal={true}
+      />
 
 
     </SafeAreaView>
@@ -116,57 +136,83 @@ const styles = StyleSheet.create({
   },
 
   flatListStyle: {
-      backgroundColor: 'white',
-      marginHorizontal: 20,
-      overflow: 'scroll',
-    },
-    sview: {
-           flexDirection: 'row',
+    backgroundColor: 'white',
+    marginHorizontal: 20,
+    overflow: 'scroll',
+  },
+  sview: {
+    flexDirection: 'row',
 
-        },
+  },
 
   image: {
     marginBottom: 5,
   },
 
-   imagek: {
-         marginBottom: 5,
-         height: 145,
-         width: 80,
-         marginLeft: 15,
-         marginTop: 1,
-       },
-    imagek2: {
-          marginBottom: 5,
-          height: 150,
-          width: 100,
-          marginRight: 260,
-          marginTop: 1,
-    },
-
-  mytext:{
-    height: 30,
-    marginBottom: 20,
+  imagek: {
+    marginBottom: 5,
+    height: 145,
+    width: 80,
+    marginLeft: 15,
+    marginTop: 1,
+  },
+  imagek2: {
+    marginBottom: 5,
+    height: 150,
+    width: 100,
+    marginRight: 260,
+    marginTop: 1,
   },
 
-  mytexta:{
-      height: 30,
-      marginTop: 0,
-      marginBottom: 0,
-      color: "green",
-    },
-     mytextb:{
-          height: 30,
-          marginTop: 0,
-          marginBottom: 0,
-          marginLeft: 0,
-          textAlign: 'center',
-          flexWrap: 'wrap',
-        },
+  mytext: {
+    height: 30,
+    marginBottom: 5,
+  },
 
-  newtext:{
+  mytexta: {
+    height: 30,
+    marginTop: 0,
+    marginBottom: 0,
+    justifyContent:'center',
+    color: "green",
+  },
+
+  goodPressureText: {
+    height: 30,
+    marginTop: 0,
+    marginBottom: 0,
+    textAlign:'center',
+    color: "green",
+  },
+
+  averagePressureText: {
+    height: 30,
+    marginTop: 0,
+    marginBottom: 0,
+    textAlign:'center',
+    color: "orange",
+  },
+
+  badPressureText: {
+    height: 30,
+    marginTop: 0,
+    marginBottom: 0,
+    textAlign:'center',
+    color: "red",
+  },
+
+  mytextb: {
+    height: 30,
+    marginTop: 0,
+    marginBottom: 0,
+    marginLeft: 0,
+    textAlign: 'center',
+    flexWrap: 'wrap',
+  },
+
+  newtext: {
     marginRight: 220,
-    marginBottom:10,
+    marginBottom: 10,
   },
 
   inputView: {
